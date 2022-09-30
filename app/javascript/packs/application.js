816272120a3a9e7913f0e01620e6f734fa5ed5eb
taigaggg <t.g.kodacc@gmail.com>
2022-09-30 13:37:38 +0900

    画像プレビュー機能の修正と実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../index") 
 require('../preview')
+require('../preview_m')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/preview_m.js
+++ app/javascript/preview_m.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('new_diary');
+  const postForm = document.getElementById('new_meal');
   // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
 
   // input要素を取得
-  const fileField = document.querySelector('input[type="file"][name="diary[image]"]');
+  const fileField = document.querySelector('input[type="file"][name="meal[image]"]');
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function(e){
     // 古いプレビューが存在する場合は削除
